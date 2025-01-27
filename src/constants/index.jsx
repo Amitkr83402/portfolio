4677b1c1771f968071d129b4bf4756a32641f0f6
Amit Kumar Yadav <amit1230kr@gmail.com>
2025-01-28 03:12:52 +0530

    Updated src/constants/index.jsx

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -1,3 +1,3 @@
 export const GITHUB_URL = "https://github.com/Amitkr83402";
 export const LINKEDIN_URL = "https://www.linkedin.com/in/amit-kumar-yadav-048b082a6/";
-export const RESUME_URL = "https://drive.google.com/file/d/1fIlQU_p6GWDmkiaxSqI7-ZgwckWfA2yB/view?usp=drivesdk";
+export const RESUME_URL = "https://docs.google.com/document/d/1_OEHQLeK0O9pBgjGp1BhgFgpXoXIX3V-/";
